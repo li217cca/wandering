ebae80c1921b6de2bdda70c78ddcbea0be9fcee9
ljrscls <446811346@qq.com>
2017-08-21 00:34:53 +0800

    webpack build env

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,45 @@
 const path = require('path');
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = function (opt = {}) {
-    console.log(opt)
+    console.log('env', process.env.NODE_ENV)
     return {
         entry: './src/index.js' ,
         output: {
             path: path.resolve(__dirname, 'dist'),
-            publicPath: "/",
-            filename: opt.dev ? 'index.js' : 'index.js?[chunkhash]'
+            // publicPath: "/",
+            filename: opt.dev ? 'index.js' : 'index.[hash].js'
         },
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+            }),
+            new webpack.EnvironmentPlugin(['NODE_ENV']),
+            new HtmlWebpackPlugin({
+                template:__dirname+'/src/index.html',
+                inject:true,
+                // chunks:['common.js']
+            }),
+            new webpack.optimize.CommonsChunkPlugin({ // 分别引用
+                name: 'common',
+                filename: 'common.[hash].js',
+            }),
+            new webpack.optimize.UglifyJsPlugin({ // 代码压缩
+                compress: {
+                    warnings: false
+                }
+            }),
+            new CleanWebpackPlugin(
+                ['dist/index.*.js','dist/common.*.js',],　 //匹配删除的文件
+                {
+                    root: __dirname,       　　　　　　　　　　//根目录
+                    verbose:  false,        　　　　　　　　　　//开启在控制台输出信息
+                    dry:      false        　　　　　　　　　　//启用删除文件
+                }
+            )
+        ],
         module: {
             rules: [{
                 test: /\.js$/,
